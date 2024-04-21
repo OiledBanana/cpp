a295ab5f7ee3f50011927bd8dfc7f74f99f23cbc
Adrian Avila <118869551+OiledBanana@users.noreply.github.com>
2024-04-21 18:27:50 +0800

    Create Numbersystem.cpp

---
+++ Chapter1/Numbersystem.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int number1 = 15; // Decimal
+    int number2 = 017; // Octal
+    int number3  = 0x0f; //hexadecimal
+    int number4 = 0b00001111; // Binary - C++14
+
+    cout << "Hello World ! " << endl;
+    cout << "Number 1: " << number1 << endl;
+    cout << "Number 2: " << number2 << endl;
+    cout << "Number 3: " << number3 << endl;
+    cout << "Number 4: " << number4 << endl;
+
+    // storing data in the memory by number system
+
+    return 0;
+}
