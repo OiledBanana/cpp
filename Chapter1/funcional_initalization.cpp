7c2d0b353159bb46763ea1852a1ea212c1f93981
Adrian Avila <118869551+OiledBanana@users.noreply.github.com>
2024-04-21 18:40:44 +0800

    Create funcional_initalization.cpp

---
+++ Chapter1/funcional_initalization.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+// functional initialization
+
+using namespace std;
+
+int main()
+ int elephant_count; //may contain garbage value
+ int lion_count(); //contains zero
+ int dog_count(10);//initializa to 10
+ int cat_count(8);//initialize to 15
+
+ int domesticated_animals (dog_count + cat_count); // can use expression as initializer
+
+ int bad_initalization (null_value1 + null_value2); // cant initialize undeclared variable
+
+ int narrowing_conversion (2.9); //2.9 is double may loss data
+
+    return 0;
+}
